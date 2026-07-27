c1f19692e9bf6a22a845861879062df4b73a5e92
Ayoub <ayoub.hamdoun@gmail.com>
2026-07-27 17:56:23 +0200

    feat: add task statistics method

--- src/services/task_manager.py
+++ src/services/task_manager.py
@@ -121,3 +121,8 @@ class TaskManager:
             Number of tasks with the given status
         """
         return len([task for task in self._tasks if task.status == status])
+
+    def get_statistics(self) -> dict:
+     """Get task statistics by status."""
+     return { "total": self.count_tasks(), "todo": self.count_by_status(TaskStatus.TODO), "in_progress": self.count_by_status(TaskStatus.IN_PROGRESS),"done": self.count_by_status(TaskStatus.DONE)}
+
